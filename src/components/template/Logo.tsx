482488db45c518ec36e0624775c0ea84c064463e
Lucas_Orleans <lucasorleans.pro@gmail.com>
2025-12-23 15:25:51 -0300

    Adicionando Logo Personalizada

---
+++ src/components/template/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo(){
+  return (
+    <div className="flex flex-col justify-center items-center h-10 w-10 rounded-full bg-white">
+      <div className="h-8 w-8 rounded-full bg-linear-to-r from-red-600 via-green-600 to-blue-700"></div>
+    </div>
+  )
+}
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,9 +1,15 @@
 import { SettingsIcon, HomeIcon, BellIcon } from "../icons";
 import ItemMenu from "./ItemMenu";
+import Logo from "./Logo";
 
 export default function SideMenu(){
   return(
     <aside>
+      <div 
+        className={`flex flex-col items-center justify-center bg-linear-to-r from-indigo-500 to-purple-800 h-20 w-20`}
+      >
+        <Logo />
+      </div>
       <ul>
         <ItemMenu url="/" text="Home" icon={HomeIcon}/>
         <ItemMenu url="/Settings" text="Settings" icon={SettingsIcon}/>
